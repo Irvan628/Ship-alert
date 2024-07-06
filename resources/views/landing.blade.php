a6b1baa4f7bb0f11eb0a8c6f129e6dce18f63045
timusss0 <mustikavia86@gmail.com>
2024-07-06 14:57:58 +0700

    trial footer

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -521,17 +521,23 @@
         </div>
     </div>
 </section>
-
-<section class="footer-section">
-        <h2><b>SHIP ALERT</b></h2>
-        <p>Bersama kami meningkatkan keselamatan maritim dengan teknologi LoRa tanpa ketergantungan internet.</p>
-        <div>
-            <img src="{{ asset('asset/instagram.png') }}" alt="Logo Email">
-            <img src="{{ asset('asset/email.png') }}" alt="Logo Instagram">
+<section class="footer-section bg-light py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="mb-3"><b>SHIP ALERT</b></h2>
+                <p>Bersama kami meningkatkan keselamatan maritim dengan teknologi LoRa tanpa ketergantungan internet.</p>
+            </div>
+            <div class="col-md-6 text-md-end">
+                <div class="d-flex justify-content-md-end mb-2">
+                    <img src="{{ asset('asset/instagram.png') }}" alt="Logo Instagram" class="me-2" style="width: 24px; height: 24px;">
+                    <img src="{{ asset('asset/email.png') }}" alt="Logo Email" style="width: 24px; height: 24px;">
+                </div>
+                <p>@2024 SHIP ALERT. All Rights Reserved.</p>
+            </div>
         </div>
     </div>
 </section>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
